a7ad0982b118b96c2d4eb4c53776752eaa779b27
Malengue <antoniocarlosmalengue@gmail.com>
2024-04-12 15:53:48 +0100

    Desafio aprimorado

--- aula1.py
+++ aula1.py
@@ -2,11 +2,11 @@ print("Digita seu nome: ")
 nome = input()
 
 print("Digita seu salário: ")
-salario = int(input())
+salario = float(input())
 
 print("Digita seu bônus: ")
 bonus = float(input())
 
 resultado = round(1000 + (salario*bonus), 2)
 
-print("Saudações "+nome+"!!! O seu salário com Bônus é "+str(resultado)+" Kz")
+print(f"Saudações {nome}!!! O seu salário com Bônus é {resultado} Kz")
