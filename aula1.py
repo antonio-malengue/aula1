440a18efb233bc21862a8127021c6d9aff2a7446
Malengue <antoniocarlosmalengue@gmail.com>
2024-04-12 15:40:56 +0100

    Desafio resolvido

--- aula1.py
+++ aula1.py
@@ -1,1 +1,12 @@
-print("Hello Word!!!")
+print("Digita seu nome: ")
+nome = input()
+
+print("Digita seu salário: ")
+salario = int(input())
+
+print("Digita seu bônus: ")
+bonus = float(input())
+
+resultado = round(1000 + (salario*bonus), 2)
+
+print("Saudações "+nome+"!!! O seu salário com Bônus é "+str(resultado)+" Kz")
